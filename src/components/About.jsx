1e1add3c1f3cfa3f6063040934296d80145bc123
kepler <47972920+alexmagwe@users.noreply.github.com>
2020-10-21 22:53:30 +0300

    added some general css styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,10 +8,10 @@ function About() {
       
     }, [setLoading])
     return (
-        <div className='about'>
-            <h3 className='about-paragraph'>
-           <p>Biblioteka which is polish meaning 'Library' is a platform for accesing course resources,it was created in 2020 and the main aim of the website was to allow easy access to course resources like pdfs          </p>
-       </h3> </div>
+        <div className='full dark'>
+            <p className='about-paragraph center-20 text-light'>
+           Biblioteka which is polish meaning 'Library' is a platform for accesing course resources,it was created in 2020 and the main aim of the website was to allow easy access to course resources like pdfs
+       </p> </div>
     )
 }
 
