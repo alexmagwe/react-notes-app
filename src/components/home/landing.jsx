a1a3f30c69fde9b277f8939e413a40e4a49878da
alexmagwe <alexmagwe@gmail.com>
2020-12-02 02:05:07 +0300

    added drag and drop functionality

--- src/components/home/landing.jsx
+++ src/components/home/landing.jsx
@@ -1,49 +1,52 @@
-import React,{useState,useEffect,useContext}from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import Search from '../search/Search.jsx'
-import {Searchcontext,Loadingcontext,Datacontext} from '../context'
-import {isEmpty} from '../helpers'
-import {allUnitsUrl,unitNotesUrl} from '../urls'
+import { Searchcontext, Loadingcontext, Datacontext } from '../context'
+import { isEmpty } from '../helpers'
+import { allUnitsUrl, unitNotesUrl } from '../urls'
 import logo from '../../images/logo.png'
 import Notes from '../notes'
 import axios from 'axios'
 
 const Landing = () => {
-    const {setLoading}=useContext(Loadingcontext)
-    const {data,setData} = useContext(Datacontext)
-    const [notes,setNotes]=useState({})
-    const {selected,movetop}=useContext(Searchcontext)
-    
-    useEffect(() => {
-        if(isEmpty(data)){
-        setLoading(true)
-        axios.get(allUnitsUrl).then(resp=>{setData(resp.data)
-        setLoading(false)}
-        )}
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        },[setLoading,setData])
+    const { setLoading } = useContext(Loadingcontext)
+    const { data, setData } = useContext(Datacontext)
+    const [notes, setNotes] = useState({})
+    const { selected, movetop } = useContext(Searchcontext)
 
     useEffect(() => {
-        
-        if (!isEmpty(selected)){
+        if (isEmpty(data)) {
             setLoading(true)
-            axios.post(unitNotesUrl,{"unit_code":selected.code}).then(resp=>{setNotes(resp.data)
-             setLoading(false)})
+            axios.get(allUnitsUrl).then(resp => {
+                setData(resp.data)
+                setLoading(false)
+            }
+            )
         }
-      
-    }, [selected,setLoading])
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [setLoading, setData])
 
-        return (
-            <div className='landing'>
-                <p className={!movetop?'landing-info':'hide'}>
-               <img  className='landing-logo' src={logo} alt='Biblioteka'/>
-                    
-                    <h4 className='text-secondary'> Access Course Resources</h4>
+    useEffect(() => {
 
-                </p>
-                <Search source={data} />
-                {!isEmpty(notes) ?(<Notes showlink={true} notes={{notes,setNotes}}/>):null}
+        if (!isEmpty(selected)) {
+            setLoading(true)
+            axios.post(unitNotesUrl, { "unit_code": selected.code }).then(resp => {
+                setNotes(resp.data)
+                setLoading(false)
+            })
+        }
+
+    }, [selected, setLoading])
+
+    return (
+        <div className='landing'>
+            <div className={!movetop ? 'landing-info' : 'hide'}>
+                <img className='landing-logo' src={logo} alt='Biblioteka' />
+                <h4 className='text-secondary'> Access Course Resources</h4>
             </div>
-        );
-    };
+            <Search source={data} />
+            {!isEmpty(notes) ? (<Notes showlink={true} notes={{ notes, setNotes }} />) : null}
+        </div>
+    );
+};
 
 export default Landing;
