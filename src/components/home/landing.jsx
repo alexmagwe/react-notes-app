005bdf4075bb802b8283fe6813c087018e899a1b
alexmagwe <alexmagwe@gmail.com>
2020-12-30 02:38:13 +0300

    changed text in landing

--- src/components/home/landing.jsx
+++ src/components/home/landing.jsx
@@ -30,7 +30,7 @@ const Landing = () => {
                     <h4 className='text-primary'> <span className='u-line'>Access all</span> Course notes and other Resources from anywhere</h4>
                 </div>
                 <div className='search-container'>
-                    <h4 className='text-primary font-20 mb-2' >Search our catalogue for hundreds of resources</h4>
+                    <h4 className='text-primary font-20 mb-2' >Search the catalogue for hundreds of resources</h4>
                     <Search source={data} />
                 </div>
             </div>
