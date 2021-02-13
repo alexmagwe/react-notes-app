5da6215af08786026525374e4a517c047844a904
alexmagwe <alexmagwe@gmail.com>
2021-02-13 20:10:12 +0300

    results for search history are synchronized with state and local storage

--- src/components/reusables/Recent.jsx
+++ src/components/reusables/Recent.jsx
@@ -4,20 +4,21 @@ import { isEmpty } from '../../helpers'
 import { Link } from 'react-router-dom'
 
 function Recent(props) {
-    const {page,unit} =props.data
+    const { page, unit } = props.data
     const { recent } = useContext(Datacontext)
     const [recentitems, setRecentItems] = useState([])
     useEffect(() => {
         if (!isEmpty(recent)) {
             setRecentItems(Object.keys(recent))
+            console.log(recent)
         }
     }
         , [recent])
     return (
         <div className='recent-container'>
             <ul className={`recent-grid ${page}-recent`}>
-                {recentitems.filter(item=>item!==unit).map((item, i) => {//hides the current unit from recent items list in unit page
-                return <li className='recent-item' key={i}><Link className='grey' to={`/unit/${recent[item].code}`} >{recent[item].name.toLowerCase()}</Link></li>
+                {recentitems.filter(item => item !== unit).map((code, i) => {//hides the current unit from recent items list in unit page
+                    return <li className='recent-item' key={i}><Link className='grey' to={`/unit/${code}`} >{recent[code].name.toLowerCase()}</Link></li>
                 })}
             </ul>
 
