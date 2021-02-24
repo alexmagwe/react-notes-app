0db443f441ff18fb9e023ff610cb36b4a1f2aa1f
alexmagwe <alexmagwe@gmail.com>
2021-02-24 12:54:44 +0300

    added video icon

--- src/components/notes/NoteItem.jsx
+++ src/components/notes/NoteItem.jsx
@@ -7,29 +7,33 @@ function NoteItem(props) {
   const note = props.item;
   const showlink = props.showlink;
   const [name, ext] = note.name.split(".");
-  const icon = geticonLink(ext);
+  let icon=null;
+  icon = geticonLink(ext);
+  if (note.category === 'video') {
+    icon = 'https://drive-thirdparty.googleusercontent.com/16/type/application/vnd.google-apps.video'
+  }
   return (
     <>
       <li className="notes-item" key={note.id}>
-        <div class='left-item-section'>
+        <div className='left-item-section'>
           {icon ? <img className='icon' src={icon} alt="" /> : null}
           <h4 className="notes-name">{name.toLowerCase()}</h4>
         </div>
         <div className='right-item-section'>
-        {note.size ? <h4>{parseInt(note.size) / 1024}</h4> : null}
-        {showlink ? (
-          <Button
-          href={`https://drive.google.com/file/d/${note.gid}/view`}
-          target="_blank"
-          >
-            <img className="link-icon" src={Link} alt="view" />
-          </Button>
-        ) : null}
-  
-          <Button target="_blank" href={`https://drive.google.com/uc?id=${note.gid}&export=download`}>
+          {note.size ? <h4>{parseInt(note.size) / 1024}</h4> : null}
+          {showlink ? (
+            <Button
+              href={`https://drive.google.com/file/d/${note.gid}/view`}
+              target="_blank"
+            >
+              <img className="link-icon" src={Link} alt="view" />
+            </Button>
+          ) : null}
+
+          {note.category !== 'video' ? <Button target="_blank" href={`https://drive.google.com/uc?id=${note.gid}&export=download`}>
             <img className="link-icon" src={Downloadicon} alt="download" />
 
-          </Button>
+          </Button> : null}
         </div>
       </li>
     </>
