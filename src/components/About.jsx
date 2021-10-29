ac1de88b28f7f95b0f48a58a3294d0eda9f05f12
alexmagwe <alexmagwe@gmail.com>
2021-10-29 09:35:02 +0300

    update:modified about paragraph

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,8 +10,10 @@ function About() {
     return (
         <div className='about-container'>
             <p className='about-paragraph font-20 font-primary'>
-                Biblioteka which is polish for 'Library' is a platform for accesing course <span className='u-line'>Notes</span>, previous exams,lecture videos and assignments
-       </p> </div>
+                Biblioteka which is polish for 'Library' is a repository for accesing course content,
+                it was made in 2020 by students for the students. The aim of the platform is to present a simple, intuitive user interface for accessing course content in a meaningful way.
+                The platform serves to provide you with all the neccessary information related to the course units you are taking or interested in.
+            </p> </div>
     )
 }
 
